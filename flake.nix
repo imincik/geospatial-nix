5366764e51529c431c31b2f112a05a63d86d9ea3
Ivan Mincik <ivan.mincik@gmail.com>
2022-12-09 19:24:18 +0100

    flake: configure Cachix binary substituter for Geonix packages

--- flake.nix
+++ flake.nix
@@ -1,6 +1,10 @@
 {
   description = "Geonix NUR repository";
+
+  nixConfig.extra-substituters = ["https://geonix.cachix.org?trusted=1"];
+
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+
   outputs = { self, nixpkgs }:
     let
       systems = [
