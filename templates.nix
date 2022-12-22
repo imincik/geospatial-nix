73c557b8265c229cc201f3d2dbe23930fe989b68
Ivan Mincik <ivan.mincik@gmail.com>
2022-12-22 21:42:30 +0100

    flake: add python-app template

--- templates.nix
+++ templates.nix
@@ -50,4 +50,62 @@
       * [Nix tutorials](https://nix.dev)
     '';
   };
+
+
+  python-app = {
+    description = "Python application";
+    path = ./templates/python-app;
+    welcomeText = ''
+      This template allows you to develop a Python application with dependencies
+      managed by Poetry.
+
+      ## Usage
+      * Add flake.nix and flake.lock files to git !
+      ```
+      git add flake.nix flake.lock
+      ```
+
+      ### Development
+      * Enter development shell (run following command in shell environment)
+      ```
+      nix develop
+      ```
+
+      * Install Python virtual environment managed by Poetry
+      ```
+      poetry install
+      ```
+
+      * Add poetry.lock file to git
+      ```
+      git add poetry.lock
+      ```
+
+      * Launch Python application development server
+      ```
+      poetry run flask run
+      ```
+
+      ### Packaging in container image
+      * Build container image
+      ```
+      nix build .#poetryAppImage
+      ```
+
+      * Load image to Docker
+      ```
+      docker load < result
+      ```
+
+      * Run container
+      ```
+      docker run -p 5000:5000 geonix-python-app
+      ```
+
+      ## More info
+      * [Nixpkgs dockerTools](https://nixos.org/manual/nixpkgs/stable/#sec-pkgs-dockerTools)
+      * [mkPoetryApplication](https://github.com/nix-community/poetry2nix#mkPoetryApplication)
+      * [Nix tutorials](https://nix.dev)
+    '';
+  };
 }
