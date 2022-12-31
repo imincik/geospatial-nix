cbe1878bcdf2a1d071c91ac8a902a27413e0f667
Ivan Mincik <ivan.mincik@gmail.com>
2022-12-31 14:57:35 +0100

    templ(python-app): show resulting shape using matplotlib

--- templates/python-app/app/__init__.py
+++ templates/python-app/app/__init__.py
@@ -1,22 +1,43 @@
 #!/usr/bin/env python
 
-import random
+import base64
+from io import BytesIO
+from random import randrange
+
 from flask import Flask
-from shapely.geometry import Point
+from matplotlib.figure import Figure
+from shapely.geometry import Point, Polygon
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def buffer():
-    x = random.randrange(1, 10)
-    y = random.randrange(1, 10)
-    p = Point(x,y)
+@app.route("/")
+def shape():
+    points = []
+    for i in range(10):
+        x = randrange(1, 100)
+        y = randrange(1, 100)
+        points.append(Point(x, y))
 
-    return str(p.buffer(2))
+    polygon = Polygon([[p.x, p.y] for p in points])
+    shape = polygon.convex_hull
+
+    x, y = shape.exterior.xy
+
+    fig = Figure()
+    ax = fig.subplots()
+    ax.plot(x, y, c="blue")
+
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+
+    return f"<img src='data:image/png;base64,{data}'/><br />{shape.wkt}"
+
 
 def run():
-   app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0")
 
-if __name__=='__main__':
-   app.run()
+
+if __name__ == "__main__":
+    app.run()
