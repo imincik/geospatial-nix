20fb2ca6af9d76193e9398afb0fb4b167dc70a0f
Ivan Mincik <ivan.mincik@gmail.com>
2023-12-05 09:01:07 +0100

    utils(pull-nixpkgs.sh): move nixpkgs-files to separate dir, apply patches

--- utils/pull-nixpkgs.sh
+++ utils/pull-nixpkgs.sh
@@ -9,13 +9,22 @@ set -Eeuo pipefail
 nixpkgs_dir=$1
 
 for pkg in pkgs/*; do
-    if [[ -f $pkg/nixpkgs-files.txt ]]; then
+    # pull files from nixpkgs
+    if [[ -f $pkg/nixpkgs/files.txt ]]; then
         echo -e "\nPulling $pkg ..."
 
         while IFS= read -r paths; do
-            eval cp -v $nixpkgs_dir/$paths
-        done < "$pkg"/nixpkgs-files.txt
+            eval cp -v "$nixpkgs_dir"/"$paths"
+        done < "$pkg"/nixpkgs/files.txt
     else
-        echo -e "\n$pkg/nixpkgs-files.txt doesn't exit. Skipping ..."
+        echo -e "\n$pkg/nixpkgs/files.txt doesn't exit. Skipping ..."
+    fi
+
+    # apply geonix patches to nixpkgs files
+    if [ 0 -lt $(ls $pkg/nixpkgs/*.patch 2>/dev/null | wc -w) ]; then
+        for patch_file in "$pkg"/nixpkgs/*.patch; do
+            echo "Applying patch $patch_file ..."
+            patch -p1 -i "$patch_file"
+        done
     fi
 done
