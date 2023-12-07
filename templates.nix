b03e2766f3f29a46b07e1c35014feb30767f18ab
Ivan Mincik <ivan.mincik@gmail.com>
2023-12-07 23:31:59 +0100

    templ: remove workspace template

--- templates.nix
+++ templates.nix
@@ -1,13 +1,4 @@
 {
-  workspace = {
-    description = "Geospatial workspace";
-    path = ./templates/workspace;
-    welcomeText = ''
-      See README.md file for instructions how to use this template.
-    '';
-  };
-
-
   python-web = {
     description = "Example Python web application";
     path = ./templates/python-web;
