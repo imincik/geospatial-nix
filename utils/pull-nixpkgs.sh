1d8b4e5d92e6cc474355cbf8b6760dfd052eefa2
Ivan Mincik <ivan.mincik@gmail.com>
2024-04-19 11:10:31 +0200

    utils(pull-nixpkgs): add nicer output indentation

--- utils/pull-nixpkgs.sh
+++ utils/pull-nixpkgs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-#
-# Pull packages from nixpkgs. Requires `nixpkgs-files.txt` file to be present in
-# geonix package directory.
+
+# Pull packages from nixpkgs.
+# Requires `files.txt` file to be present in `pkgs/<PACKAGE>/nixpkgs` directory.
 # Usage: pull-packages.sh <NIXPKGS-DIR>
 
 set -Eeuo pipefail
@@ -14,17 +14,17 @@ for pkg in pkgs/*; do
         echo -e "\nPulling $pkg ..."
 
         while IFS= read -r paths; do
-            eval cp -v "$nixpkgs_dir"/"$paths"
+            eval cp -v "$nixpkgs_dir"/"$paths" | sed -e 's/^/  /'
         done < "$pkg"/nixpkgs/files.txt
     else
-        echo -e "\n$pkg/nixpkgs/files.txt doesn't exit. Skipping ..."
+        echo -e "\n  $pkg/nixpkgs/files.txt doesn't exit. Skipping ..."
     fi
 
     # apply geonix patches to nixpkgs files
     if [ 0 -lt $(ls $pkg/nixpkgs/*.patch 2>/dev/null | wc -w) ]; then
         for patch_file in "$pkg"/nixpkgs/*.patch; do
-            echo "Applying patch $patch_file ..."
-            patch -p1 -i "$patch_file"
+            echo "  Applying patch $patch_file ..."
+            patch -p1 -i "$patch_file" | sed -e 's/^/    /'
         done
     fi
 done
